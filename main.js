dce69a2ea9b004c26e77c3580e9fcb803446923b
Trini Morrison <TRPMorrison@gmail.com>
2015-05-11 13:50:49 -0400

    UPDATES TO FILES

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 var givenName;
 // Q: What does `givenName` equal right now?
-
+// A: givenName is undefined
 
 givenName = "Tim";
 // Q: What is `givenName` set to right now?
-
+// A: "Tim"
 
 givenName = "JD";
 // Q: What is `givenName` set to right now?
+// A: "JD"
 
 
 var greeting = "Hello, how are you?";
 // Q: What is `greeting` set to?
+// A: greeting is undefined
 
 
 var high = 50;
@@ -20,21 +22,32 @@ var low  = 10;
 
 var math = high - low;
 // Q: What is `math` set to?
+// A: 40
 
 
 math = high - "5";
 // Q: What is `math` set to?
+// A: 45
 
 
 // Create a variable to calculate Tim's age
 // The answer should read "Tim is 31 years old"
 var born = 1983;
 var today = 2015;
-
+//  A: var a = 'Tim is ';
+//     var b = (today - born);
+//     var c = '-1';
+//     var d = ' years old';
+//     a + (b + c) + d
 
 
 // Store some information following in variables.
 var yourName;
+//  var yourName = 'Trinidad Morrison';
+//  console.log(yourName);
 var instructorName;
-
+//  var instructorName = 'JD Isaacks';
+//  console.log(instructorName);
 var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+//  var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+//  console.log(statement);
